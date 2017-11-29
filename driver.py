32a3191d160d24912d510904e4b6fb8d08a9a32f
Vedmurty Chavan <ved1@umbc.edu>
2017-11-29 18:47:54 -0500

    Driver.py updated. Made OOP like

--- driver.py
+++ driver.py
@@ -1,56 +1,82 @@
 import numpy as np
-# import matplotlib.pyplot as plt
-from sklearn import linear_model
-
 # srcCode = "D:/Study/ACA/project/sample1.py"
 from sample1 import runCode
 import os
 import pickle
+import logisticRegression as lr
 
-DEBUG = 0
-BRANCHES = 10
-selectedBranch = 0
+DEBUG     = 0
+ITERATION = 5
+NUM_OF_BRANCHES = 10
 
-def logisticRegression(x, y):
-	modelFile = "lr_model.sav"
+class BranchPredictor(object):
 	
-	if (not os.path.isfile(modelFile)):
-		lr = linear_model.LogisticRegression()
-		prediction = [-1]*BRANCHES
-	else:
-		lr = pickle.load(open(modelFile, 'rb'))
-		prediction = lr.predict(x)
+	def __init__(self):
+		self.GHR          = [0]*NUM_OF_BRANCHES
+		self.p_pred       = [0]*NUM_OF_BRANCHES
+		self.lr_pred      = [0]*NUM_OF_BRANCHES
+		self.lvq_pred     = [0]*NUM_OF_BRANCHES
+		self.accuracy     = {"perceptron" : 0, 
+		                     "logistic"   : 0,
+		                     "LVQ"        : 0
+							}
+
+	def perceptron_predict(self):
+		self.p_pred = [1]*NUM_OF_BRANCHES # Give call to Perceptron
+		pass 
+
+	def perceptron_learn(self):
+		pass
+
+	def lr_predict(self):
+		self.lr_pred = lr.LRPredict(self.GHR)
+
+	def lr_learn(self, actOutput):
+		lr.LRLearn([self.GHR], actOutput)
+
+	def lvq_predict(self):
+		pass
+
+	def lvq_learn(self):
+		pass
+
+	def updateGHR(self, output):
+		for i in range(NUM_OF_BRANCHES):
+			self.GHR[i] += output[i]
+
+	def calculateDiff(self, output):
+		print("ACTUAL VS EXPECTED ", self.lr_pred, output)
+		self.accuracy["perceptron"] += sum(map(lambda x, y : x*y==1, self.p_pred,   output)) / NUM_OF_BRANCHES
+		self.accuracy["logistic"]   += sum(map(lambda x, y : x*y==1, self.lr_pred,  output)) / NUM_OF_BRANCHES
+		self.accuracy["LVQ"]        += sum(map(lambda x, y : x*y==1, self.lvq_pred, output)) / NUM_OF_BRANCHES
+
+	def calculateAccuracy(self, iterations):
+		print("The Accuracies of the models for ",iterations, " iterations are: ")
+		print(" Perceptron                   : ", self.accuracy["perceptron"] / iterations)
+		print(" Logistic Regression          : ", self.accuracy["logistic"]   / iterations) 
+		print(" Learning Vector Quantization : ", self.accuracy["LVQ"]        / iterations)
+
+
+bp = BranchPredictor()
+
+for _ in range(ITERATION):
+
+	# Get the 3 predictions
+	bp.perceptron_predict()
+	bp.lr_predict()
+	bp.lvq_predict()
 	
-	print("Current GHR : ", x, " Predicted Value :", prediction, " Actual Value : ", y)
+	# Run the actual Code
+	actualOutput = runCode()[0]
 
-	lr.fit(x, y)
-	pickle.dump(lr, open(modelFile, 'wb'))
+	# Send the actual O/P to model for updation
+	# bp.perceptron_learn(actualOutput)
+	bp.lr_learn(actualOutput)
+	# bp.lvq_learn(actualOutput)
+	bp.calculateDiff(actualOutput)
 
+	# Update GHR for next iterations
+	bp.updateGHR(actualOutput)
 
-GHR = [0] * BRANCHES
+bp.calculateAccuracy(ITERATION)
 
-for _ in range(10):
-	# Actual Output (All branches)
-	x = runCode()
-	# Actual Output (Selected branch)
-	y = [x[0][selectedBranch]]
-
-	# TODO Send current GHR to predictor to get the prediction for this branch.
-	logisticRegression(GHR, y)
-
-	# Update the GHR with actual outcome
-	GHR = [GHR[i] + x[0][i] for i in range(BRANCHES)]
-
-
-
-# createInputData()
-
-def createInputData():
-	x = runCode()
-	branchNo = 0
-	y = [b[branchNo] for b in x]
-
-	if(DEBUG):
-		print(x, y)
-
-	logisticRegression(x, y)
